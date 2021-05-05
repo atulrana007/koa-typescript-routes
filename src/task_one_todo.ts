eee25c1b5159b1b4bdf1ab091324294f1a138125
atul.rana <atul.rana@zopsmart.com>
2021-05-05 15:53:51 +0530

    adds task 0 and task 1

---
+++ src/task_one_todo.ts
@@ -0,0 +1,94 @@
+import * as Koa from "koa";
+import * as Router from "koa-router";
+import { DefaultContext, DefaultState, ParameterizedContext } from "koa";
+import * as bodyParser from "koa-bodyparser";
+import * as render from "koa-ejs";
+import * as serve from "koa-static";
+import "colors";
+import * as dotenv from "dotenv";
+import * as path from "path";
+import * as fs from "fs";
+// import * as methodOverride from "koa-methodoverride";
+dotenv.config({ path: __dirname + "/.env" });
+
+const port = process.env.PORT || 5000;
+const staticDir = path.join(__dirname + "/..", "public/css");
+console.log(staticDir);
+
+// const StyleCss: object = {
+//   style: fs.readFileSync(path.join(__dirname + "/..", "public/css/styles.css")),
+// };
+// console.log(StyleCss);
+const app: Koa<DefaultState, DefaultContext> = new Koa();
+const router: Router = new Router();
+
+app.use(bodyParser());
+// app.use(methodOverride("_method"));
+app.use(serve(staticDir));
+
+const items: Array<{ id: number; value: string }> = [
+  { id: 0, value: "Do Something" },
+];
+
+render(app, {
+  root: path.join(__dirname + "/..", "views"),
+  layout: "layout",
+  viewExt: "html",
+  cache: false,
+  debug: false,
+});
+
+router.get(
+  "/",
+  async (ctx: ParameterizedContext<DefaultState, DefaultContext>) => {
+    await ctx.render("index", {
+      title: "To Do List",
+      items: items,
+    });
+  }
+);
+
+// router.get;
+
+router.get(
+  "/addItems",
+  async (ctx: ParameterizedContext<DefaultState, DefaultContext>) => {
+    await ctx.render("add");
+  }
+);
+// router.get(
+//   "/public/css/styles.css",
+//   async (ctx: ParameterizedContext<DefaultState, DefaultContext>) => {
+//     ctx.body = StyleCss;
+//   }
+// )
+
+const add = async (ctx: ParameterizedContext<DefaultState, DefaultContext>) => {
+  const body: any = ctx.request.body;
+  const item: string = body.item;
+  const itemId: number = items.length;
+  items.push({ id: itemId, value: item });
+  ctx.redirect("/");
+};
+
+router.post("/addItems", add);
+
+router.del(
+  "/deleteItems/id",
+  async (ctx: ParameterizedContext<DefaultState, DefaultContext>) => {
+    const deleteId: number = ctx.params.id;
+    const index: number = items.findIndex((ele) => {
+      return ele.id === deleteId;
+    });
+    console.log("deleting Index", index);
+    if (index !== -1) items.splice(index, 1);
+    console.log(items);
+    ctx.redirect("/");
+  }
+);
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(port).on("listening", () => {
+  console.log(`Listening at port ${port}...`);
+});
---
+++ src/task_zero_simple_routes.ts
@@ -0,0 +1,44 @@
+import * as Koa from "koa";
+import { DefaultState, DefaultContext, ParameterizedContext } from "koa";
+import * as Router from "koa-router";
+import * as dotenv from "dotenv";
+import "colors";
+
+dotenv.config({ path: __dirname + "/.env" });
+
+const port = process.env.PORT || 5000;
+
+const app: Koa<DefaultState, DefaultContext> = new Koa();
+const router: Router = new Router();
+
+router.get(
+  "/",
+  async (ctx: ParameterizedContext<DefaultState, DefaultContext>) => {
+    ctx.body = { msg: "Task 1" };
+  }
+);
+router.get(
+  "/hello",
+  async (ctx: ParameterizedContext<DefaultState, DefaultContext>) => {
+    ctx.body = "world";
+  }
+);
+router.get(
+  "/echo",
+  async (ctx: ParameterizedContext<DefaultState, DefaultContext>) => {
+    ctx.body = ctx.query;
+  }
+);
+router.get(
+  "/error",
+  async (ctx: ParameterizedContext<DefaultState, DefaultContext>) => {
+    ctx.status = 500;
+    ctx.body = "Internal Server Error";
+  }
+);
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(port).on("listening", () => {
+  console.log(`Listening at port ${port}...`);
+});
