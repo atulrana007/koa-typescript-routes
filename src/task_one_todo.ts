ce6020960ee8487e3cf8a5cfc28f37374c8f0ca3
atul.rana <atul.rana@zopsmart.com>
2021-05-05 16:34:27 +0530

    updated the code with delete and put request

--- src/task_one_todo.ts
+++ src/task_one_todo.ts
@@ -26,7 +26,7 @@ app.use(bodyParser());
 // app.use(methodOverride("_method"));
 app.use(serve(staticDir));
 
-const items: Array<{ id: number; value: string }> = [
+let items: Array<{ id: number; value: string }> = [
   { id: 0, value: "Do Something" },
 ];
 
@@ -68,21 +68,37 @@ const add = async (ctx: ParameterizedContext<DefaultState, DefaultContext>) => {
   const item: string = body.item;
   const itemId: number = items.length;
   items.push({ id: itemId, value: item });
+  ctx.response.body = "Added Successfully";
   ctx.redirect("/");
 };
 
 router.post("/addItems", add);
 
 router.del(
-  "/deleteItems/id",
+  "/deleteItem/id",
   async (ctx: ParameterizedContext<DefaultState, DefaultContext>) => {
     const deleteId: number = ctx.params.id;
     const index: number = items.findIndex((ele) => {
       return ele.id === deleteId;
     });
     console.log("deleting Index", index);
-    if (index !== -1) items.splice(index, 1);
+    items = index !== -1 ? items.splice(index, 1) : items;
     console.log(items);
+    ctx.redirect("/");
+  }
+);
+
+router.put(
+  "/updateItem/:id",
+  async (ctx: ParameterizedContext<DefaultState, DefaultContext>) => {
+    const updateId: number = ctx.params.id;
+    const body: any = ctx.request.body;
+    const item: string = body.item;
+    const index: number = items.findIndex((ele) => {
+      return ele.id == updateId;
+    });
+    console.log("final_index", index);
+    if (index !== -1) items[index].value = item;
     ctx.redirect("/");
   }
 );
