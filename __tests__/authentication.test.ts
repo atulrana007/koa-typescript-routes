3e2d91722b81e67062e4043bdd5acb85d2b83b76
atul.rana <atul.rana@zopsmart.com>
2021-05-25 19:05:37 +0530

    pipeline temporary fix

--- __tests__/authentication.test.ts
+++ __tests__/authentication.test.ts
@@ -29,23 +29,23 @@ describe("Auth Routes test", () => {
     const responseTwo = await request(server)
       .post("/users")
       .send({ name: "Atul", password: "deepu123" });
-    // expect(responseTwo.status).toEqual(500);
-    // expect(responseTwo.body.message).toEqual("user already exits");
+    expect(responseTwo.status).toEqual(500);
+    expect(responseTwo.body.message).toEqual("user already exits");
   });
   test("should check auth route get", async () => {
     const response = await request(server)
       .post("/users")
       .send({ name: "Deepu", password: "deepu123" });
-    // expect(response.status).toEqual(200);
+    expect(response.status).toEqual(200);
     const responseGet = await request(server).get("/users");
-    // expect(responseGet.status).toEqual(200);
-    // expect(responseGet.body[0].name).toEqual("Atul");
+    expect(responseGet.status).toEqual(200);
+    expect(responseGet.body[0].name).toEqual("Atul");
   });
   test("should login user if valid name or password are added", async () => {
     const response = await request(server)
       .post("/users")
       .send({ name: "Ankit", password: "Ankit123" });
-    // expect(response.status).toEqual(200);
+    expect(response.status).toEqual(200);
     const accessToken = response.body.token;
 
     const responseLogin = await request(server)
@@ -53,21 +53,21 @@ describe("Auth Routes test", () => {
       .set("authorization", `Bearer ${accessToken}`)
       .send({ name: "Ankit", password: "Ankit123" });
 
-    // expect(responseLogin.status).toEqual(200);
+    expect(responseLogin.status).toEqual(200);
   });
 
   test("should throw error if invalid name or password are added", async () => {
     const response = await request(server)
       .post("/users")
       .send({ name: "Shaurya", password: "shaurya123" });
-    // expect(response.status).toEqual(200);
+    expect(response.status).toEqual(200);
     const accessToken = response.body.token;
 
     const responseLogin = await request(server)
       .post("/login")
       .send({ name: "Shaurya", password: "Ankit123" })
       .set("authorization", accessToken);
-    // expect(responseLogin.status).toEqual(500);
-    // expect(responseLogin.body.message).toEqual("Invalid Username or password");
+    expect(responseLogin.status).toEqual(500);
+    expect(responseLogin.body.message).toEqual("Invalid Username or password");
   });
 });
